b3454e5e8f12cfa47ecbb400e1523db9eff8ce90
Dylan Zelmar <dylanzelmar@gmail.com>
2026-05-09 01:34:16 +0200

    Add C# Assembly project

---
+++ Sources/DyzeCoreExample/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace Dyze.RimWorld.CoreExample
+{
+    public class DyzeCoreExampleMod: Mod
+    {
+        public DyzeCoreExampleMod(ModContentPack content) : base(content)
+        {
+            Log.Message("[DyzeCoreExample] Mod assembly loaded successfully.");
+        }
+    }
+
+}
+
